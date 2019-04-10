5377b7508fc62be9f1904316888efabb6a510d54
nostayup <lierlong@buaa.edu.cn>
2019-04-10 13:36:54 +0800

    record common commands

--- goodluck.py
+++ goodluck.py
@@ -5,4 +5,16 @@ Created on Wed Apr 10 13:13:14 2019
 @author: lierl
 """
 
-print('good luck')
+print('操作步骤')
+print('==========')
+print('初始化文件夹：git init')
+print('添加文件到缓存区：git add filename')
+print('提交修改到工作区：git commit -m "what you want to record"')
+print('连接远程库：git remote add origin  git@github.com:nostayup/XXXXXXX.git')
+print('如果远程库和本地库不一致，先让远程文件同步到本地，执行：')
+print('git pull --rebase origin master')
+print('提交到远程库：git push -u origin master')
+
+
+
+
