732be7baf253ae974c4e03e23d3d9f221c8e124e
nostayup <lierlong@buaa.edu.cn>
2019-04-10 13:39:26 +0800

    record common commands

--- goodluck.py
+++ goodluck.py
@@ -7,13 +7,14 @@ Created on Wed Apr 10 13:13:14 2019
 
 print('操作步骤')
 print('==========')
-print('初始化文件夹：git init')
-print('添加文件到缓存区：git add filename')
-print('提交修改到工作区：git commit -m "what you want to record"')
-print('连接远程库：git remote add origin  git@github.com:nostayup/XXXXXXX.git')
+print('初始化文件夹：\ngit init')
+print('添加文件到缓存区：\ngit add filename')
+print('提交修改到工作区：\ngit commit -m "what you want to record"')
+print('显示工作区和缓存区的区别：\ngit diff')
+print('连接远程库：\ngit remote add origin  git@github.com:nostayup/XXXXXXX.git')
 print('如果远程库和本地库不一致，先让远程文件同步到本地，执行：')
 print('git pull --rebase origin master')
-print('提交到远程库：git push -u origin master')
+print('提交到远程库：\ngit push -u origin master')
 
 
 
